c7ea9bf41c37a3e0d5af984189f38c429720576e
Hannah Hendrickson <hannahhendrickson@Hannahs-MBP.PK5001Z>
2017-08-16 13:09:55 -0600

    Added lots of test and json files

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
-import {DataProvider} from './container';
-import { BrowserRouter as Router} from 'react-router-dom';
 
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-          <Router>
-            <DataProvider />
-          </Router>
+      <div className="App">
+        <p>Hello WOrld</p>
+      </div>
     );
   }
 }
